8d30f2d55b6169c639ff651a674a0536dada2da5
JerraH <jerra.haynes@gmail.com>
2018-05-20 15:25:26 -0400

    feed working again, linked user pages

--- client/components/blogs/blog-entry.jsx
+++ client/components/blogs/blog-entry.jsx
@@ -1,15 +1,19 @@
 import React, { Component } from 'react';
+import {Link} from 'react-router-dom';
 
 
-function BlogEntry(props) { return (
-<div className="post card" id={props.post.id}>
+function BlogEntry(props) {
+  let post = props.post;
+  let user = props.post.user
+  return (
+<div className="post card" id={post.id}>
   <div className="card-title">
     <img src="#" />
-    <h4>{props.post.user.username}</h4>
+    <h4><Link to={`/users/${user.id}`} key={user.id}>{user.username}</Link></h4>
   </div>
   <div className="card-body postText">
-    <h3>{props.post.title ? props.post.title : null}</h3>
-    {props.post.content}
+    <h3>{post.title ? post.title : null}</h3>
+    {post.content}
   </div>
   <div className="buttonholder">
     <div className="left">
--- client/components/blogs/blog-feed.jsx
+++ client/components/blogs/blog-feed.jsx
@@ -14,6 +14,7 @@ class BlogFeed extends Component {
   }
 
   render() {
+    console.log(this.props.blogFeed)
     return (
       <div className="container">
         { this.props.blogFeed.map(post => {
